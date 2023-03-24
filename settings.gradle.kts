13f341f6f56e545e85876dade2232dedf5d5982a
Jason Manuel <jama.indo@hotmail.com>
2023-03-24 04:49:10 +0000

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name="idris-micronaut"
+
---
+++ src/main/kotlin/com/jasonmanuel/idrismicronaut/Application.kt
@@ -0,0 +1,7 @@
+package com.jasonmanuel.idrismicronaut
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
